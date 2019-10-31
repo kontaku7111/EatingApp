e39b47875c14659c06ae011a304fa06f78475efd
takumi <kontaku7111@gmail.com>
2019-10-31 16:32:31 +0900

    make it possible to put button and change a debugging text

--- app/src/main/java/jp/ac/agu/wil/MainActivity.java
+++ app/src/main/java/jp/ac/agu/wil/MainActivity.java
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView DebugMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DebugMessage=findViewById(R.id.DebugMessage);
     }
 
     public void BtnClick(View view) {
         if (view.getId()==R.id.start_button) {
-
+            DebugMessage.setText("You push a Start button");
+        }else if(view.getId()==R.id.stop_button){
+            DebugMessage.setText("You push a Stop button");
         }
-
     }
 }
